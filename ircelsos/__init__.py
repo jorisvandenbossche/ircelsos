001bed00845972728aa46c369333a8cdb8ead33c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2015-11-03 00:06:09 +0100

    Update setup.py for 0.1 release

--- ircelsos/__init__.py
+++ ircelsos/__init__.py
@@ -5,6 +5,8 @@ Created on Wed Apr 07 23:11:39 2015
 @author: Joris Van den Bossche
 """
 
+__version__ = "0.1.0"
+
 from .sos import get_sos
 
 SOS = get_sos()
--- setup.py
+++ setup.py
@@ -2,17 +2,27 @@
 
 """
 
+import os
+from io import open
 from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='ircelsos',
-    version='0.1.dev',
+    version='0.1.0',
     description='Downloading air quality data from the SOS of IRCEL - CELINE',
-    #long_description=long_description,
-    url='https://github.ugent.be/jorvdnbo/ircelsos',
+    long_description=long_description,
+    url='https://github.com/jorisvandenbossche/ircelsos',
     author='Joris Van den Bossche',
     author_email='jorisvandenbossche@gmail.com',
-    #license='MIT',
+    license='BSD',
     packages=['ircelsos'],
     install_requires=['owslib==0.9.0', 'six'],
     entry_points={
