620c978cc350bead8fe1e0548898fb963ef97124
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-07-06 11:10:23 +0200

    RLS: ircelsos 0.2.2
    New release for mistake in uploading to PyPI

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ircelsos',
-    version='0.2.1',
+    version='0.2.2',
     description='Downloading air quality data from the SOS of IRCEL - CELINE',
     long_description=long_description,
     url='https://github.com/jorisvandenbossche/ircelsos',
